0ec80b30f2a8dd8df765cea73ef1fc5fc682b754
Mike Farr <mike@tokenize.ca>
2017-08-02 09:10:57 -0400

    Add timestamp parameter

--- evl/notifiers/consolenotifier.py
+++ evl/notifiers/consolenotifier.py
@@ -1,11 +1,13 @@
-import datetime
+from datetime import datetime
 
-class ConsoleNotifier():
-    def __init__(self, format_str=""):
+
+class ConsoleNotifier:
+    def __init__(self, format_str: str=None):
         self.format_str = format_str
-        if len(self.format_str) == 0:
+        if format_str is None:
             self.format_str = "[{timestamp}] Event: {event}"
 
-    def notify(self, event):
-        timestamp = datetime.datetime.now()
+    def notify(self, event, timestamp=None):
+        if timestamp is None:
+            timestamp = datetime.now()
         print(self.format_str.format(timestamp=timestamp, event=event))
