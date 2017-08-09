117d75e60ec5626616c69b55c87b991af160a9f7
Mike Farr <mike@tokenize.ca>
2017-08-09 19:18:19 -0400

    Minor formatting change

--- evl/notifiers/consolenotifier.py
+++ evl/notifiers/consolenotifier.py
@@ -1,13 +1,16 @@
 from datetime import datetime
 
 
+from evl.event import Event
+
+
 class ConsoleNotifier:
     def __init__(self, format_str: str=None):
         self.format_str = format_str
         if format_str is None:
-            self.format_str = "[{timestamp}] Event: {event}"
+            self.format_str = "[{timestamp}] {event}"
 
-    def notify(self, event, timestamp=None):
+    def notify(self, event: Event, timestamp=None):
         if timestamp is None:
             timestamp = datetime.now()
         print(self.format_str.format(timestamp=timestamp, event=event))
