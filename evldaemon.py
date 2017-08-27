131745ee48d4f2ad9b2ff8494f010cc09f3335e3
Mike Farr <mike@tokenize.ca>
2017-08-27 12:09:06 -0400

    Add initial JSON configuration support

--- evldaemon.py
+++ evldaemon.py
@@ -1,3 +1,6 @@
+import argparse
+import json
+import os.path
 import sys
 import socket
 import signal
@@ -7,21 +10,60 @@ from evl.command import Priority
 from evl.connection import Connection
 from evl.notifiers.consolenotifier import ConsoleNotifier
 
+
+def read_config(file: str) -> dict:
+    file = os.path.expanduser(file)
+
+    data = None
+    try:
+        with open(file) as json_data:
+            data = json.load(json_data)
+    except FileNotFoundError:
+        print("Configuration file '{file}' not found!".format(file=file))
+    except json.JSONDecodeError:
+        print("Invalid configuration file '{file}'!".format(file=file))
+
+    return data
+
 if __name__ == '__main__':
     print("Welcome to EvlDaemon")
 
-    if len(sys.argv) < 3:
-        print("Usage: evldaemon.py <host> <password>")
-        sys.exit()
+    p = argparse.ArgumentParser()
+    p.add_argument('-c', '--config', required=False, default="~/.evldaemon/config.json")
+    options = p.parse_args()
+    config = read_config(options.config)
 
-    host = sys.argv[1]
-    password = sys.argv[2]
+    if config is None:
+        print("Unable to read configuration file. Exiting.")
+        exit(1)
+
+    host = config.get('ip')
+    if host is None:
+        print("IP address not found in configuration file!")
+        exit(1)
+
+    password = config.get('password')
+    if password is None:
+        print("Password not found in configuration file!")
+        exit(1)
 
     resolved = socket.gethostbyname(host)
-
     connection = Connection(host=resolved, password=password)
-    console = ConsoleNotifier(priority=Priority.LOW)
-    connection.event_manager.add_notifier(console)
+
+    for notifier in config.get('notifiers', []):
+        # Set up notifiers defined in configuration file
+        priority = Priority[notifier.get('priority', 'LOW').upper()]
+
+        if notifier['type'] == 'console':
+            new_notifier = ConsoleNotifier(priority=priority)
+        else:
+            new_notifier = None
+
+        if new_notifier:
+            connection.event_manager.add_notifier(new_notifier)
+
+    connection.event_manager.zones = config.get('zones', {})
+    connection.event_manager.partitions = config.get('partitions', {})
 
     gevent.signal(signal.SIGINT, connection.stop)
 
