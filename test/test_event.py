54af683011d392fe19c09b31d9caa76ebc25fb9f
Mike Farr <mike@tokenize.ca>
2018-01-30 13:21:10 -0500

    Use correct reference dicts

--- test/test_event.py
+++ test/test_event.py
@@ -1,30 +1,38 @@
 import unittest
 
-from evl import command as cmd
-from evl import event
-from evl.util import merge_dicts
+import evl.command as cmd
+import evl.event as ev
+import evl.util as util
 
 
 class TestEventManager(unittest.TestCase):
 
     def test_command_describe_default(self):
         command = cmd.Command(cmd.CommandType.LOGIN.value)
-        desc = event.COMMAND_NAMES[command.command_type]
-        mgr = event.EventManager(None)
+        desc = cmd.NAMES[command.command_type]
+        mgr = ev.EventManager(None)
 
         self.assertEqual(desc, mgr._describe_command(command))
 
     def test_command_describe_override(self):
         command = cmd.Command(cmd.CommandType.LOGIN.value)
         cmd_names = {cmd.CommandType.LOGIN: "LOGIN!"}
-        mgr = event.EventManager(None)
-        event.EventManager.command_names = merge_dicts(event.COMMAND_NAMES, cmd_names)
+        cmd.NAMES = util.merge_dicts(cmd.NAMES, cmd_names)
+        mgr = ev.EventManager(None)
 
         self.assertEqual("LOGIN!", mgr._describe_command(command))
 
     def test_led_state_describe(self):
         state = "83"
         state_str = "Backlight, Armed, Ready"
-        mgr = event.EventManager(None)
+        mgr = ev.EventManager(None)
 
         self.assertEqual(state_str, mgr._describe_led_state(state))
+
+    def test_event_priority_override(self):
+        cmd_priorities = {cmd.CommandType.LOGIN: cmd.Priority.CRITICAL}
+        cmd.PRIORITIES = util.merge_dicts(cmd.PRIORITIES, cmd_priorities)
+        command = cmd.Command(cmd.CommandType.LOGIN.value)
+        event = ev.Event(command, {})
+
+        self.assertEqual(cmd.Priority.CRITICAL, event.priority)
