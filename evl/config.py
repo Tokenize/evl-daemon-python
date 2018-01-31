0877f27a9674f97738dd16f964fe689a368c0e51
Mike Farr <mike@tokenize.ca>
2018-01-30 20:44:35 -0500

    Refactor core logic into separate class

---
+++ evl/config.py
@@ -0,0 +1,91 @@
+import json
+import os
+
+import evl.command as cmd
+import evl.notifiers.consolenotifier as console
+import evl.notifiers.smsnotifier as sms
+import evl.notifiers.emailnotifier as email
+import evl.storage.memory as memory
+
+
+DEFAULT_STORAGE_MAX_LENGTH = 100
+
+
+def load_notifiers(config: list) -> list:
+    """
+    Load notifiers from given list of notifier configurations
+    :param config: List of notifier configuration dicts
+    """
+    notifiers = []
+    for notifier in config:
+        priority = cmd.Priority[notifier.get('priority', 'LOW').upper()]
+        name = notifier.get('name')
+        kind = notifier['type']
+
+        if kind == 'console':
+            new_notifier = console.ConsoleNotifier(priority=priority, name=name)
+        elif kind == 'sms':
+            layout = notifier.get('layout')
+            settings = notifier.get('settings')
+            sender = settings.get('sender')
+            recipient = settings.get('recipient')
+            sid = settings.get('sid')
+            auth_token = settings.get('authToken')
+            new_notifier = sms.SmsNotifier(sid, auth_token, sender, recipient,
+                                           priority, layout, name)
+        elif kind == 'email':
+            layout = notifier.get('layout')
+            settings = notifier.get('settings')
+            sender = settings.get('sender')
+            recipient = settings.get('recipient')
+            api_key = settings.get('apiKey')
+            subject = settings.get('subject')
+            new_notifier = email.EmailNotifier(api_key, sender, recipient, priority,
+                                               layout, subject, name)
+        else:
+            new_notifier = None
+
+        if new_notifier:
+            notifiers.append(new_notifier)
+    return notifiers
+
+
+def load_storage(config: list) -> list:
+    """
+    Load storage engines from given list of storage configurations
+    :param config: List of storage engine configuration dicts
+    """
+    storages = []
+    for storage in config:
+        kind = storage['type']
+        # Set up storage engines defined in configuration file
+        if kind == 'memory':
+            max_size = storage.get('maxSize', DEFAULT_STORAGE_MAX_LENGTH)
+            new_storage = memory.MemoryStorage(size=max_size)
+        else:
+            new_storage = None
+
+        if new_storage:
+            storages.append(new_storage)
+    return storages
+
+
+def read(file: str) -> dict:
+    """
+    Reads configuration from given file path and returns dictionary of
+    configuration objects.
+    :param file: Configuration file path
+    :return: Dictionary of configuration objects
+    """
+    file = os.path.expanduser(file)
+
+    data = None
+    try:
+        with open(file) as json_data:
+            data = json.load(json_data)
+    except FileNotFoundError:
+        print("Configuration file '{file}' not found!".format(file=file))
+    except json.JSONDecodeError:
+        print("Invalid configuration file '{file}'!".format(file=file))
+
+    return data
--- evldaemon.py
+++ evldaemon.py
@@ -1,105 +1,65 @@
 import argparse
-import json
-import os.path
 import socket
 import signal
 import gevent.pool
 import gevent.queue
 import gevent.signal
 
-from evl.command import Priority
-from evl.connection import Connection
-from evl.event import EventManager
-from evl.notifiers.consolenotifier import ConsoleNotifier
-from evl.notifiers.smsnotifier import SmsNotifier
-from evl.notifiers.emailnotifier import EmailNotifier
-from evl.storage.memory import MemoryStorage
+import evl.connection as conn
+import evl.event as ev
+import evl.config as conf
 
 
-DEFAULT_STORAGE_MAX_LENGTH = 100
+class EvlDaemon:
+    def __init__(self, host: str, password: str, port: int, config: dict=None):
+
+        self.host = host
+        self.password = password
+        self.port = port
+
+        if config is None:
+            config = {}
+        self.config = config
+
+        self.notifiers = conf.load_notifiers(self.config.get('notifiers', []))
+        self.storage = conf.load_storage(self.config.get('storage', []))
+
+        self.queue_group = gevent.pool.Group()
+        self.event_queue = gevent.queue.Queue()
+
+        self.event_manager = ev.EventManager(self.event_queue, self.queue_group)
+        self.event_manager.add_notifiers(self.notifiers)
+        self.event_manager.add_storages(self.storage)
+
+    def start(self):
+        resolved = socket.gethostbyname(host)
+        connection = conn.Connection(event_manager=self.event_manager,
+                                queue_group=self.queue_group,
+                                host=resolved,
+                                password=password)
+
+        # Assign zone and partition names as read from configuration file.
+        ev.EventManager.zones = config.get('zones', {})
+        ev.EventManager.partitions = config.get('partitions', {})
+
+        # TODO: Read command name, priority, login name, etc. overrides from config.
+
+        # Are multiple signal handlers possible?
+        gevent.signal(signal.SIGINT, connection.stop)
+        gevent.signal(signal.SIGINT, self.queue_group.kill)
+
+        connection.start()
+        self.queue_group.join()
 
 
-def read_config(file: str) -> dict:
-    """
-    Reads configuration from given file path and returns dictionary of
-    configuration objects.
-    :param file: Configuration file path
-    :return: Dictionary of configuration objects
-    """
-    file = os.path.expanduser(file)
+if __name__ == '__main__':
+    print("Welcome to EvlDaemon")
 
-    data = None
-    try:
-        with open(file) as json_data:
-            data = json.load(json_data)
-    except FileNotFoundError:
-        print("Configuration file '{file}' not found!".format(file=file))
-    except json.JSONDecodeError:
-        print("Invalid configuration file '{file}'!".format(file=file))
-
-    return data
-
-def load_notifiers(config: list):
-    """
-    Load notifiers from given list of notifier configurations
-    :param list: List of notifier configuration dicts
-    """
-    notifiers = []
-    for notifier in config:
-        priority = Priority[notifier.get('priority', 'LOW').upper()]
-        name = notifier.get('name')
-        kind = notifier['type']
-
-        if kind == 'console':
-            new_notifier = ConsoleNotifier(priority=priority, name=name)
-        elif kind == 'sms':
-            layout = notifier.get('layout')
-            settings = notifier.get('settings')
-            sender = settings.get('sender')
-            recipient = settings.get('recipient')
-            sid = settings.get('sid')
-            auth_token = settings.get('authToken')
-            new_notifier = SmsNotifier(sid, auth_token, sender, recipient, priority, layout, name)
-        elif kind == 'email':
-            layout = notifier.get('layout')
-            settings = notifier.get('settings')
-            sender = settings.get('sender')
-            recipient = settings.get('recipient')
-            api_key = settings.get('apiKey')
-            subject = settings.get('subject')
-            new_notifier = EmailNotifier(api_key, sender, recipient, priority, layout, subject, name)
-        else:
-            new_notifier = None
-
-        if new_notifier:
-            notifiers.append(new_notifier)
-    return notifiers
-
-def load_storage(config: list):
-    """
-    Load storage engines from given list of storage configurations
-    :param list: List of storage engine configuration dicts
-    """
-    storages = []
-    for storage in config:
-        kind = storage['type']
-        # Set up storage engines defined in configuration file
-        if kind == 'memory':
-            max_size = storage.get('maxSize', DEFAULT_STORAGE_MAX_LENGTH)
-            new_storage = MemoryStorage(size=max_size)
-        else:
-            new_storage = None
-
-        if new_storage:
-            storages.append(new_storage)
-    return storages
-
-def start():
-    """Start the main evl-daemon process"""
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--config', required=False, default="~/.evldaemon/config.json")
+    parser.add_argument('-c', '--config', required=False,
+                        default="~/.evldaemon/config.json")
     options = parser.parse_args()
-    config = read_config(options.config)
+    config = conf.read(options.config)
 
     if config is None:
         print("Unable to read configuration file. Exiting.")
@@ -115,36 +75,8 @@ def start():
         print("Password not found in configuration file!")
         exit(1)
 
-    queue_group = gevent.pool.Group()
-    event_queue = gevent.queue.Queue()
-    event_manager = EventManager(event_queue, queue_group)
+    port = config.get('port', 4025)
 
-    resolved = socket.gethostbyname(host)
-    connection = Connection(event_manager=event_manager,
-                            queue_group=queue_group,
-                            host=resolved,
-                            password=password)
+    ed = EvlDaemon(host, password, port, config)
+    ed.start()
 
-    notifiers = load_notifiers(config.get('notifiers', []))
-    event_manager.add_notifiers(notifiers)
-
-    storages = load_storage(config.get('storage', []))
-    event_manager.add_storages(storages)
-
-    # Assign zone and partition names as read from configuration file.
-    EventManager.zones = config.get('zones', {})
-    EventManager.partitions = config.get('partitions', {})
-
-    # TODO: Read command name, priority, login name, etc. overrides from config.
-
-    # Are multiple signal handlers possible?
-    gevent.signal(signal.SIGINT, connection.stop)
-    gevent.signal(signal.SIGINT, queue_group.kill)
-
-    connection.start()
-    queue_group.join()
-
-
-if __name__ == '__main__':
-    print("Welcome to EvlDaemon")
-    start()
