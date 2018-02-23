07ddfc85e1e371f2e1e6bf2ee891494375996c47
Mike Farr <mike@tokenize.ca>
2018-02-22 22:02:38 -0500

    Add authorization checking to requests

--- evl/listeners/http.py
+++ evl/listeners/http.py
@@ -7,11 +7,12 @@ logger = logging.getLogger(__name__)
 
 
 class HttpListener:
-    def __init__(self, name: str, port: int, event_manager):
+    def __init__(self, name: str, port: int, auth_token: str, event_manager):
         self.app = flask.Flask(__name__)
 
         self.name = name
         self.port = port
+        self.auth_token = auth_token
         self.event_manager = event_manager
 
         self.app.add_url_rule('/status_report', '/status_report', self.status_report)
@@ -19,10 +20,16 @@ class HttpListener:
     def __str__(self):
         return self.name
 
+    def _authorize(self):
+        auth_token = flask.request.args.get('auth_token')
+        if auth_token != self.auth_token:
+            return flask.abort(403)
+
     def listen(self):
         logger.debug("Starting HTTP listener...")
         server = wsgi.WSGIServer(('', self.port), self.app)
         server.serve_forever()
 
     def status_report(self):
+        self._authorize()
         return flask.jsonify(self.event_manager.status_report())
