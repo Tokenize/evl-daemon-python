3451618f5862553baabc9330186d423fc43fce4e
Mike Farr <mike@tokenize.ca>
2018-02-26 20:22:34 -0500

    Add data parsing tests

--- test/test_data.py
+++ test/test_data.py
@@ -1,5 +1,6 @@
 import unittest
 
+import evl.command as cmd
 import evl.data as dt
 
 
@@ -9,3 +10,24 @@ class TestData(unittest.TestCase):
         state_str = "Backlight, Armed, Ready"
 
         self.assertEqual(state_str, dt.describe_led_state(state))
+
+    def test_zone_data_parse(self):
+        command = cmd.Command("609")
+        data = "001"
+        parsed = dt.parse(command, data)
+
+        self.assertEqual(parsed, {'zone': '001', 'data': ''})
+
+    def test_zone_partition_data_parse(self):
+        command = cmd.Command("601")
+        data = "1001"
+        parsed = dt.parse(command, data)
+
+        self.assertEqual(parsed, {'partition': '1', 'zone': '001', 'data': ''})
+
+    def test_data_parse(self):
+        command = cmd.Command("510")
+        data = "83"
+        parsed = dt.parse(command, data)
+
+        self.assertEqual(parsed, {'data': data})
